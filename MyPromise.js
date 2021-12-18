ebac5587182b8f75d6e4159a5bff8d37f7580d1c
xudaobing <xudaobing@Office.sdwan>
2021-12-18 22:53:11 +0800

    update

--- MyPromise.js
+++ MyPromise.js
@@ -1,11 +1,14 @@
 class MyPromise {
   // 状态 pending || fulfilled || rejected
   #PromiseState;
+  // 
   #PromiseResult;
 
   constructor(fn) {
     // 初始化
-    this.#init();
+    this.#PromiseState = 'pending';
+    this.#PromiseResult = undefined;
+
     try {
       fn((res) => this.resolve(res), (err) => this.reject(err));
     } catch (error) {
@@ -13,22 +16,19 @@ class MyPromise {
     }
   }
 
-  #init() {
-    this.#PromiseState = 'pending';
-    this.#PromiseResult = undefined;
-  }
   get isPending() {
     return this.#PromiseState === 'pending';
   }
   resolve(res) {
+    // 只有pending状态下 能改变状态为fulfilled
     if (!this.isPending) return this;
     this.#PromiseState = 'fulfilled';
     this.#PromiseResult = res;
   }
   reject(err) {
+    // 只有pending状态下 能改变状态为rejected
     if (!this.isPending) return this;
     this.#PromiseState = 'rejected';
     this.#PromiseResult = err;
-    // throw(err);
   }
 }
